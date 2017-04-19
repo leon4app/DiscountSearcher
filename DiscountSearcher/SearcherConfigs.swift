27ea05de7796906f44e47a61e68b3e27c308f67c
LeonTse <466489438@qq.com>
2017-04-19 18:59:49 +0800

    Declare class SearcherConfigs

---
+++ DiscountSearcher/SearcherConfigs.swift
@@ -0,0 +1,20 @@
+//
+//  SearcherConfigs.swift
+//  DiscountSearcher
+//
+//  Created by LeonTse on 2017/4/19.
+//  Copyright © 2017年 LeonTse. All rights reserved.
+//
+
+import UIKit
+
+class SearcherConfigs: NSObject
+{
+    var requestURL: URL?
+    
+    open func getConfigs(searcher:Searcher, type: AnyObject) -> URL?
+    {
+        return nil
+    }
+    
+}
