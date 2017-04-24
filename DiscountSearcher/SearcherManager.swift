daf555828dfc66adb2ffa8104d2950cc5438e691
LeonTse <466489438@qq.com>
2017-04-24 11:49:08 +0800

    Declare SearcherManager

---
+++ DiscountSearcher/SearcherManager.swift
@@ -0,0 +1,33 @@
+//
+//  SearcherManager.swift
+//  DiscountSearcher
+//
+//  Created by LeonTse on 2017/4/24.
+//  Copyright © 2017年 LeonTse. All rights reserved.
+//
+
+import UIKit
+
+class SearchManager
+{
+    var searchers : Array<Searcher>?
+    var keyword : String?
+    
+    func openApp(keyword: String, url: URL)
+    {
+        
+    }
+    func createMetaData() -> SearchMetaData?
+    {
+        return nil
+    }
+    func search(metaData: SearchMetaData)
+    {
+        
+    }
+    
+    func cancel()
+    {
+        
+    }
+}
