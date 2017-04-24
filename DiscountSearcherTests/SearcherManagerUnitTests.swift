f62ed1b1d0796fde9b38d9398b8400d2487e4931
LeonTse <466489438@qq.com>
2017-04-24 15:57:44 +0800

    1. Updates unit test. 2. rename unit test file name as suggestion.

--- DiscountSearcherTests/SearcherManagerUnitTests.swift
+++ DiscountSearcherTests/SearcherManagerUnitTests.swift
@@ -1,32 +1,24 @@
 //
-//  SearcherConfigsUnitTest.swift
+//  SearcherManagerUnitTests.swift
 //  DiscountSearcher
 //
-//  Created by LeonTse on 2017/4/19.
+//  Created by LeonTse on 2017/4/24.
 //  Copyright © 2017年 LeonTse. All rights reserved.
 //
 
 import XCTest
 @testable import DiscountSearcher
 
-class SearcherConfigsUnitTest: XCTestCase {
+class SearcherManagerUnitTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-    
-    func testSearcherConfigs()
-    {
-        let a = MeituanSearcher.init(keyword: "s", address: "s")
-        let url = SearcherConfigs.sharedInstance.getConfigs(searcher: a, type: "s" as AnyObject)
-        XCTAssertEqual(url, URL(string: "http://www.dianping.com/search/category/7/10/g103")!)
     }
     
     func testExample() {
